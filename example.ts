7f4f465972bf184ae68349b68595329c21f8ed17
tomoki-yamamura <progtab17@gmail.com>
2024-04-10 15:06:11 +0800

    partial

--- example.ts
+++ example.ts
@@ -1,3 +1,17 @@
-function addNumbers(a, b) {
-  return a+ b
+interface User {
+  name: string;
+  email: string;
+  age?: number;
 }
+
+let user1: User = {
+  name: "john",
+  email: "hoge@gmail",
+  age: 18,
+};
+
+function updateUser(user: User, fieldsToUpdate: Partial<User>) {
+  return {...user, ...fieldsToUpdate}
+}
+
+console.log(updateUser(user1, {email: "hgoe" }));
