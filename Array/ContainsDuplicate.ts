c14918495c79c4f32376456313a691c16a66bbd6
tomoki-yamamura <progtab17@gmail.com>
2024-04-15 01:32:47 +0800

    product

--- Array/ContainsDuplicate.ts
+++ Array/ContainsDuplicate.ts
@@ -1,11 +1,5 @@
 function containsDuplicate(nums: number[]): boolean {
-  const numberMap = new Map<number, boolean>()
-  for(let i = 0; i < nums.length; i++) {
-    if (numberMap.has(nums[i])) {
-      return true
-    }
-    numberMap.set(nums[i], true)
-  }
-  return false
+  const store: Set<number> = new Set([...nums]);
+  return store.size !== nums.length;
 };
 
---
+++ Array/ProductofArrayExceptSelf.ts
@@ -0,0 +1,27 @@
+function productExceptSelf(nums: number[]): number[] {
+  const result = new Array(nums.length).fill(1);
+  let prefix = 1;
+  let postfix = 1;
+  for (let i = 0; i < nums.length; ++i) {
+      result[i] *= prefix
+      result[nums.length - 1 - i] *= postfix;
+      prefix *= nums[i]
+      postfix *= nums[nums.length - 1 - i];
+  }
+  return result;
+}
+
+console.log(productExceptSelf([1,2,3,4]));
+
+// Example 1:
+
+// Input: nums = [1,2,3,4,5]
+//               [1,2,6,24,120]
+//               [120,120,60,20,5]
+//               [120,60,40,30,24]
+//               [24,24,12,4]
+// Output: [24,12,8,6]
+// Example 2:
+
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
