63ae2e8bc63d6b22a8c108db12554ca197180376
tomoki-yamamura <progtab17@gmail.com>
2024-04-21 02:23:30 +0800

    coin change

---
+++ DynamicPrograming/CoinChange.ts
@@ -0,0 +1,15 @@
+function coinChange(coins: number[], amount: number): number {
+  const dp: number[] = new Array(amount+1).fill(Infinity);
+  dp[0] = 0
+
+  for (let i = 1; i <= amount; i++) {
+      for (let coin of coins) {
+        if (i - coin >= 0) {
+          dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+        }
+      }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+}
+
+console.log(coinChange([1,2,5], 11));
