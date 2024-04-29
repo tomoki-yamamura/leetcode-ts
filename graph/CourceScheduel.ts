04d3500a7fbd87d094091c3961c73a94bf5b9caa
tomoki-yamamura <progtab17@gmail.com>
2024-04-30 03:02:40 +0800

    cource scheduel

--- graph/CourceScheduel.ts
+++ graph/CourceScheduel.ts
@@ -1,35 +1,69 @@
-const detectCycle = (course: number, classesNeeded: number[][], visited: boolean[]) => {
-  const reqs = classesNeeded[course];
+// const detectCycle = (course: number, classesNeeded: number[][], visited: boolean[]) => {
+//   const reqs = classesNeeded[course];
   
+//   if (visited[course]) return true;
+//   if (reqs.length === 0) return false;
+
+//   visited[course] = true;
+
+//   while (reqs.length) {
+//       if (detectCycle(reqs[reqs.length - 1], classesNeeded, visited)) return true;
+//       reqs.pop();
+//   }
+
+//   visited[course] = false;
+//   return false;
+// }
+
+const detectCycle = (course: number, classesNeeded: number[][], visited: boolean[]): boolean => {
+  const reqs = classesNeeded[course]
   if (visited[course]) return true;
-  if (!reqs.length) return false;
+  if (reqs.length === 0) return false;
 
   visited[course] = true;
 
-  while (reqs.length) {
-      if (detectCycle(reqs[reqs.length - 1], classesNeeded, visited)) return true;
-      reqs.pop();
+  while(reqs.length > 0) {
+    if (detectCycle(reqs[reqs.length-1], classesNeeded, visited)) return true;
+    reqs.pop();
   }
-
-  visited[course] = false;
-  return false;
+  visited[course] = false
+  return false
 }
 
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
   if (prerequisites.length === 0) return true
   const classesNeeded: number[][] = Array.from({length: numCourses}, () => []);
   prerequisites.forEach(([course, prereq]) => {
-    classesNeeded[course].push(prereq)
+    classesNeeded[course].push(prereq);
   })
-
-  const visited = Array.from({ length: numCourses}, () => false);
+  const visited = Array.from({length: numCourses}, () => false);
   for (const [course, _] of prerequisites) {
-    if (detectCycle(course, classesNeeded, visited)) return false
-}
+    if(detectCycle(course, classesNeeded, visited)) return false
+  }
   return true
-};
+}
 
-const n = 5
-const prerequisites = [[0,1],[0,2],[1,3],[1,4],[3,4]]
+// function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+//   if (prerequisites.length === 0) return true
+//   const classesNeeded: number[][] = Array.from({length: numCourses}, () => []);
+//   prerequisites.forEach(([course, prereq]) => {
+//     classesNeeded[course].push(prereq)
+//   })
 
-canFinish(5, prerequisites)
+//   const visited = Array.from({ length: numCourses}, () => false);
+//   for (const [course, _] of prerequisites) {
+//     if (detectCycle(course, classesNeeded, visited)) return false
+// }
+//   return true
+// };
+
+
+
+// const n = 5
+// const prerequisites = [[0,1],[0,2],[1,3],[1,4],[3,4]]
+
+// canFinish(5, prerequisites)
+
+export {
+  canFinish
+}
--- index.ts
+++ index.ts
@@ -1,22 +1,5 @@
-import cloneGraph from "./graph/CloneGraph";
+import { canFinish } from "./graph/CourceScheduel"
+const n = 5
+const prerequisites = [[0,1],[0,2],[1,3],[1,4],[3,4]]
 
-// Definition for Node.
-class Node {
-  val: number
-  neighbors: Node[]
-  constructor(val?: number, neighbors?: Node[]) {
-      this.val = (val===undefined ? 0 : val)
-      this.neighbors = (neighbors===undefined ? [] : neighbors)
-  }
-}
-
-const nodes: Node[] = [];
-for (let i = 0; i < 4; i++) {
-    nodes.push(new Node(i + 1));
-}
-nodes[0].neighbors.push(nodes[1], nodes[3]);
-nodes[1].neighbors.push(nodes[0], nodes[2]);
-nodes[2].neighbors.push(nodes[1], nodes[3]);
-nodes[3].neighbors.push(nodes[0], nodes[2]);
-
-cloneGraph(nodes[0])
+canFinish(5, prerequisites)
