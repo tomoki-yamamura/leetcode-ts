b5f333fe324e9ea246ce8cd8608be97da3fe789b
tomoki-yamamura <progtab17@gmail.com>
2024-05-01 23:23:12 +0800

    fin numcourse

--- graph/CourceScheduel.ts
+++ graph/CourceScheduel.ts
@@ -1,69 +1,85 @@
-// const detectCycle = (course: number, classesNeeded: number[][], visited: boolean[]) => {
-//   const reqs = classesNeeded[course];
-  
+// const detectCycle = (course: number, classesNeeded: number[][], visited: boolean[]): boolean => {
+//   const reqs = classesNeeded[course]
 //   if (visited[course]) return true;
 //   if (reqs.length === 0) return false;
 
 //   visited[course] = true;
 
-//   while (reqs.length) {
-//       if (detectCycle(reqs[reqs.length - 1], classesNeeded, visited)) return true;
-//       reqs.pop();
+//   while(reqs.length > 0) {
+//     if (detectCycle(reqs[reqs.length-1], classesNeeded, visited)) return true;
+//     reqs.pop();
 //   }
-
-//   visited[course] = false;
-//   return false;
+//   visited[course] = false
+//   return false
 // }
-
-const detectCycle = (course: number, classesNeeded: number[][], visited: boolean[]): boolean => {
-  const reqs = classesNeeded[course]
-  if (visited[course]) return true;
-  if (reqs.length === 0) return false;
-
-  visited[course] = true;
-
-  while(reqs.length > 0) {
-    if (detectCycle(reqs[reqs.length-1], classesNeeded, visited)) return true;
-    reqs.pop();
-  }
-  visited[course] = false
-  return false
-}
-
-function canFinish(numCourses: number, prerequisites: number[][]): boolean {
-  if (prerequisites.length === 0) return true
-  const classesNeeded: number[][] = Array.from({length: numCourses}, () => []);
-  prerequisites.forEach(([course, prereq]) => {
-    classesNeeded[course].push(prereq);
-  })
-  const visited = Array.from({length: numCourses}, () => false);
-  for (const [course, _] of prerequisites) {
-    if(detectCycle(course, classesNeeded, visited)) return false
-  }
-  return true
-}
 
 // function canFinish(numCourses: number, prerequisites: number[][]): boolean {
 //   if (prerequisites.length === 0) return true
 //   const classesNeeded: number[][] = Array.from({length: numCourses}, () => []);
 //   prerequisites.forEach(([course, prereq]) => {
-//     classesNeeded[course].push(prereq)
+//     classesNeeded[course].push(prereq);
 //   })
-
-//   const visited = Array.from({ length: numCourses}, () => false);
+//   const visited = Array.from({length: numCourses}, () => false);
 //   for (const [course, _] of prerequisites) {
-//     if (detectCycle(course, classesNeeded, visited)) return false
-// }
+//     if(detectCycle(course, classesNeeded, visited)) return false
+//   }
 //   return true
-// };
+// }
 
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+  const preMap: Map<number, number[]> = new Map();
+  const visited: Map<number, boolean> = new Map();
 
+  for (let i = 0; i < prerequisites.length; i++) {
+    const [course, prerequisite] = prerequisites[i]
+    if (!preMap.get(course)) {
+      preMap.set(course, [prerequisite]);
+    } else {
+      const prereqs = preMap.get(course)!
+      prereqs.push(prerequisite)
+      preMap.set(course, prereqs)
+    }
+  }
 
-// const n = 5
-// const prerequisites = [[0,1],[0,2],[1,3],[1,4],[3,4]]
+  const dfs = (node: number) => {
+    if (visited.get(node)) {
+      return false;
+    }
+    if (preMap.get(node) !== undefined) {
+      if (preMap.get(node)!.length === 0) {
+        return true;
+      }
 
-// canFinish(5, prerequisites)
+      visited.set(node, true);
+      for (let val of preMap.get(node)!) {
+        if (!dfs(val)) {
+          return false;
+        }
+      }
+      visited.set(node, false);
 
-export {
-  canFinish
+      preMap.set(node, []);
+    }
+    return true;
+  };
+
+  for (let key of preMap.keys()) {
+    if (!dfs(key)) {
+      return false;
+    }
+  }
+  return true;
 }
+
+const n = 5;
+const prerequisites = [
+  [0, 1],
+  [0, 2],
+  [1, 3],
+  [1, 4],
+  [3, 4],
+];
+
+canFinish(5, prerequisites);
+
+export { canFinish };
