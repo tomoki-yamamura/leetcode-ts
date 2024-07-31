2193183e609b0e798c29bd86ec90325073c91ab8
tomoki-yamamura <progtab17@gmail.com>
2024-07-31 21:30:42 +0800

    largest number

--- Array/LargestNumber.ts
+++ Array/LargestNumber.ts
@@ -1,3 +1,10 @@
 function largestNumber(nums: number[]): string {
-  
+  nums.sort((a, b) => {
+    let sa = a.toString()
+    let sb = b.toString()
+
+    return parseInt(sa + sb) > parseInt(sb + sa) ? -1 : 1
+  })
+  if (nums[0] === 0) return '0'
+  return nums.join('')
 };
