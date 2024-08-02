eace8b356f37b36ff692b90ce8375d5cb5fdf022
tomoki-yamamura <progtab17@gmail.com>
2024-08-02 22:07:58 +0800

    findRotain

--- Array/DetermineWhetherMatrix.ts
+++ Array/DetermineWhetherMatrix.ts
@@ -1,3 +1,18 @@
 function findRotation(mat: number[][], target: number[][]): boolean {
-    
+  let width = mat[0].length
+  let height = mat.length
+
+  let normal = true;
+  let right90 = true;
+  let right180 = true;
+  let right270 = true;
+
+  for(let i = 0; i < height; i++) {
+    for(let j = 0; j < width; j++) {
+      if (mat[i][j] !== target[i][j]) {
+        normal = false
+      }
+      if (mat[i][j] !== target[j])
+    }
+  }
 };
