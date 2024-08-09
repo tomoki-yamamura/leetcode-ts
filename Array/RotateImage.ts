76ca546940029e855b3d147f497b7bcb47281f62
tomoki-yamamura <progtab17@gmail.com>
2024-08-09 20:28:38 +0800

    spiral

--- Array/RotateImage.ts
+++ Array/RotateImage.ts
@@ -1,4 +1,4 @@
- export function rotate(matrix: number[][]): void {
+export function rotate(matrix: number[][]): void {
   let n = matrix.length;
   let depth = Math.floor(n / 2); // 中心からの深さを計算
 
@@ -7,9 +7,9 @@
     let opp = n - 1 - i;
     for(let j = 0; j < len; j++) {
       let tematrixp = matrix[i][i+j]
-      matrix[i][i + j] = matrix[opp - j][i];
-      matrix[opp - j][i] = matrix[opp][opp - j];
-      matrix[opp][opp - j] = matrix[i + j][opp];
+      matrix[i][i+j] = matrix[opp-j][i]
+      matrix[opp-j][i] = matrix[opp][opp - j]
+      matrix[opp][opp-j] = matrix[i+j][opp]
       matrix[i + j][opp] = tematrixp;
     }
   }
