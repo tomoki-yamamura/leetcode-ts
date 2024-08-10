d54bc8cbedbb97b59d2164e7c17c15c2974a6f83
tomoki-yamamura <progtab17@gmail.com>
2024-08-11 02:17:13 +0800

    spiral

--- Array/SpiralMatrix.ts
+++ Array/SpiralMatrix.ts
@@ -1,3 +1,39 @@
 function spiralOrder(matrix: number[][]): number[] {
-  const w = matrix.length;
+  const vertical = matrix.length;
+  const horizontal = matrix[0].length;
+
+  let result = []
+
+  let top = 0
+  let right = horizontal - 1
+  let bottom = vertical - 1
+  let left = 0;
+
+  while(top <= bottom && left <= right) {
+    for(let i = left; i <= right; i++) {
+      result.push(matrix[top][i])
+    }
+    top++
+
+    for(let i = top; i <= bottom; i++) {
+      result.push(matrix[i][right])
+    }
+    right--
+
+    if (top <= bottom) {
+      for(let i = right; left <= i; i--) {
+        result.push(matrix[bottom][i])
+      }
+      bottom--
+    }
+
+    if (left <= right) {
+      for(let i = bottom; top <= bottom; i--) {
+        result.push(matrix[i][left])
+      }
+      left++
+    }
+  }
+
+  return result
 };
