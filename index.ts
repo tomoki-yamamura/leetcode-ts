47d191f1b4e2783bfc4673c2ff5b14f199759705
tomoki-yamamura <progtab17@gmail.com>
2024-08-12 20:04:26 +0800

    valid parentheses

--- index.ts
+++ index.ts
@@ -1,14 +1,8 @@
 import { diagonalSum } from "./Array/MatrixDiagonalSum";
 import { rotate } from "./Array/RotateImage";
+import { isValid } from "./Array/ValidParentheses";
 
 
 
-
-const resu = rotate([
-  [ 1,  2,  3,  4,  5],
-  [ 6,  7,  8,  9, 10],
-  [11, 12, 13, 14, 15],
-  [16, 17, 18, 19, 20],
-  [21, 22, 23, 24, 25]
-])
+const resu = isValid("([{])}")
 console.log(resu);
