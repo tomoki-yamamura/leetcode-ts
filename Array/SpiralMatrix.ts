6be372b74cfc67f636f33b90460c084ae37eb29c
tomoki-yamamura <progtab17@gmail.com>
2024-08-12 18:30:20 +0800

    valid parentheses

--- Array/SpiralMatrix.ts
+++ Array/SpiralMatrix.ts
@@ -1,3 +1,43 @@
+// function spiralOrder(matrix: number[][]): number[] {
+//   const vertical = matrix.length;
+//   const horizontal = matrix[0].length;
+
+//   let result = []
+
+//   let top = 0
+//   let right = horizontal - 1
+//   let bottom = vertical - 1
+//   let left = 0;
+
+//   while(top <= bottom && left <= right) {
+//     for(let i = left; i <= right; i++) {
+//       result.push(matrix[top][i])
+//     }
+//     top++
+
+//     for(let i = top; i <= bottom; i++) {
+//       result.push(matrix[i][right])
+//     }
+//     right--
+
+//     if (top <= bottom) {
+//       for(let i = right; left <= i; i--) {
+//         result.push(matrix[bottom][i])
+//       }
+//       bottom--
+//     }
+
+
+//     if (left <= right) {
+//       for (let i = bottom; i >= top; i--) {
+//         result.push(matrix[i][left]);
+//       }
+//       left++;
+//     }
+//   }
+
+//   return result
+// };
 function spiralOrder(matrix: number[][]): number[] {
   const vertical = matrix.length;
   const horizontal = matrix[0].length;
@@ -27,11 +67,12 @@ function spiralOrder(matrix: number[][]): number[] {
       bottom--
     }
 
+
     if (left <= right) {
-      for(let i = bottom; top <= bottom; i--) {
-        result.push(matrix[i][left])
+      for (let i = bottom; i >= top; i--) {
+        result.push(matrix[i][left]);
       }
-      left++
+      left++;
     }
   }
 
