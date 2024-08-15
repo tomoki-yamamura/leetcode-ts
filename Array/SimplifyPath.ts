44a797ed5496bb37729e178f033c7f8eac295f0f
tomoki-yamamura <progtab17@gmail.com>
2024-08-15 20:55:56 +0800

    simplify

--- Array/SimplifyPath.ts
+++ Array/SimplifyPath.ts
@@ -1,3 +1,19 @@
 function simplifyPath(path: string): string {
-    
+  const splitted = path.split("/").filter(i => i !== "")
+  const result: string[] = []
+
+  splitted.forEach((v, i) => {
+    if (v === "..") {
+      result.pop()
+    }
+    if (v !== ".." && v !== ".") {
+      result.push(v)
+    }
+  })
+  return "/" + result.join('/')
 };
+
+// const resu = 
+// simplifyPath("/home/")
+
+// console.log(resu);
