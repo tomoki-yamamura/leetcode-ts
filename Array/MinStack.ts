22aebde695d0e28345a9be01cd70af92413fd91d
tomoki-yamamura <progtab17@gmail.com>
2024-08-18 18:33:28 +0800

    min stack

--- Array/MinStack.ts
+++ Array/MinStack.ts
@@ -1,24 +1,45 @@
 class MinStack {
+  private stack: number[]
+
   constructor() {
-      
+    this.stack = []
   }
 
   push(val: number): void {
-      
+      this.stack.push(val)
   }
 
   pop(): void {
-      
+      this.stack.pop()
   }
 
   top(): number {
-      
+    return [...this.stack].sort((a, b) => b - a)[0]
   }
 
   getMin(): number {
-      
+    return [...this.stack].sort((a, b) => a - b)[0]
   }
 }
+
+// Example 1:
+
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output
+// [null,null,null,null,-3,null,0,-2]
+
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
 
 /**
 * Your MinStack object will be instantiated and called as such:
