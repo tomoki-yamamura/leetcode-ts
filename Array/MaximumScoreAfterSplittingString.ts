dae8d8b82d5a13ff9351dd7ceab16dd50e8ae6dc
tomoki-yamamura <progtab17@gmail.com>
2024-08-20 21:08:08 +0800

    add rightshift

--- Array/MaximumScoreAfterSplittingString.ts
+++
@@ -1,3 +0,0 @@
-function maxScore(s: string): number {
-    
-};
---
+++ Array/ProductExceptSelf.ts
@@ -0,0 +1,16 @@
+function productExceptSelf(nums: number[]): number[] {
+  const rightShift = []
+  
+};
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// [1,2,3,4]
+// [1,2,6,24]
+// [24,24,12,4]
+// Output: [24,12,8,6]
+// Example 2:
+
+// Input: nums = [-1,1,0,-3,3]
+// Output: [0,0,9,0,0]
