b5f1bbe899b0f00dd914b049be480576c6815f07
tomoki-yamamura <progtab17@gmail.com>
2024-08-21 00:27:18 +0800

    add rightshift

--- Array/ProductExceptSelf.ts
+++ Array/ProductExceptSelf.ts
@@ -1,7 +1,18 @@
 function productExceptSelf(nums: number[]): number[] {
-  const rightShift = []
-  
+  const result = new Array(nums.length).fill(1);
+  let prefix = 1;
+  let postfix = 1;
+  for(let i = 0; i < nums.length; i++) {
+    result[i] *= prefix
+    result[nums.length-1-i] *= postfix
+    prefix *= nums[i]
+    postfix *= nums[nums.length - 1 - i];
+  }
+
+  return result
 };
+
+productExceptSelf([1,2,3,4])
 
 // Example 1:
 
