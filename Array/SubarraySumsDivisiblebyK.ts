ae61cc220b238d70d49aa5facb88fba4f37d98e9
tomoki-yamamura <progtab17@gmail.com>
2024-08-26 19:51:32 +0800

    sum

--- Array/SubarraySumsDivisiblebyK.ts
+++ Array/SubarraySumsDivisiblebyK.ts
@@ -1,3 +1,26 @@
 function subarraysDivByK(nums: number[], k: number): number {
-    
+
+  let map = []
+  map[0] = 1
+  let count = 0
+  let total = 0
+  for (const num of nums) {
+    count = (count + num) % k
+    if(count<0) count +=k;
+    if(map[count]) total += map[count]
+    map[count] = map[count] ? map[count] + 1 : 1
+  }
+
+  return total
 };
+
+// Example 1:
+
+// Input: nums = [4,5,0,-2,-3,1], k = 5
+// Output: 7
+// Explanation: There are 7 subarrays with a sum divisible by k = 5:
+// [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+// Example 2:
+
+// Input: nums = [5], k = 9
+// Output: 0
