090e24a65207d914e30fdd786a7466399ddfa6a4
tomoki-yamamura <progtab17@gmail.com>
2024-09-02 20:29:19 +0800

    remove element

--- Array/RemoveElement.ts
+++ Array/RemoveElement.ts
@@ -1,3 +1,10 @@
 function removeElement(nums: number[], val: number): number {
-    
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        // If the element is not val
+        if (nums[i] !== val) {
+            nums[count++] = nums[i];
+        }
+    }
+    return count;
 };
