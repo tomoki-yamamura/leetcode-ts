b5b0abf1a3b7fd6b2c01c85ab5071b9a0e2ceca8
tomoki-yamamura <progtab17@gmail.com>
2024-09-08 18:06:07 +0800

    kradius subarray

--- Array/KRadiusSubarrayAverages.ts
+++ Array/KRadiusSubarrayAverages.ts
@@ -1,3 +1,15 @@
 function getAverages(nums: number[], k: number): number[] {
-    
+  let sum = 0;
+  for(let i = 0; i<nums.length; i++) {
+    if(i <= k && k <= nums.length-i) {
+      
+    } else {
+      sum += nums[i]
+      nums[i] = -1
+    }
+  }
 };
+
+
+// Input: nums = [7,4,3,9,1,8,5,2,6], k = 3
+// Output: [-1,-1,-1,5,4,4,-1,-1,-1]
