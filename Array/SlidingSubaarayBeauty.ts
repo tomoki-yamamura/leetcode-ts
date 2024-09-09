be1681b65254a5f8b6829925067ad738f1d173c9
tomoki-yamamura <progtab17@gmail.com>
2024-09-09 22:53:58 +0800

    getSubarrayBeauty first attempt

--- Array/SlidingSubaarayBeauty.ts
+++ Array/SlidingSubaarayBeauty.ts
@@ -1,16 +1,29 @@
-function getSubarrayBeauty(nums: number[], k: number, x: number): number[] {
-  const slice = nums.slice(0, k);
-  const sortedSubaaray = slice.sort((a, b) => a - b);
-  const result = [];
-  for (let i = 0; i <= nums.length - k; i++) {
-    const slice = nums.slice(i, k);
-    const sortedSubaaray = slice.sort((a, b) => a - b);
-    result.push(valueCalc(sortedSubaaray, x));
+export function getSubarrayBeauty(nums: number[], k: number, x: number): number[] {
+  const n = nums.length;
+  const f = new Map();
+  for(let R = -50; R < n; R++) {
+    f.set(R, 0)
   }
-  return result;
-}
 
-function valueCalc(subaaray: number[], x: number) {
-  if (subaaray[0] >= 0) return 0;
-  return subaaray[x - 1];
+  const result = []
+  let L = 0;
+  for(let R = 0; R < n; R++) {
+    f.set(nums[R], f.get(nums[R] + 1))
+    if (R < k - 1) continue
+    let total = 0;
+    for (let R2 = -50; R2 <= 50; R2++) {
+      if (f.get(R2)) {
+        total += f.get(R2)
+        if (total >= x) {
+          if (R2 < 0) {
+            result.push(R2)
+          } else {
+            result.push(0)
+          }
+          break
+        }
+      }
+    }
+  }
+  return result
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
-import { getAverages } from "./Array/KRadiusSubarrayAverages";
+import { getSubarrayBeauty } from "./Array/SlidingSubaarayBeauty";
 
-const resu = getAverages([7,4,3,9,1,8,5,2,6], 3)
-console.log(resu);
+
+const nums = [1,-1,-3,-2,3]
+const k = 3
+const x = 2
+
+console.log(getSubarrayBeauty(nums, k, x));
