378ed8c86ae569c9687a220ba7e53c56e3f3f210
tomoki-yamamura <progtab17@gmail.com>
2024-09-09 20:56:32 +0800

    getSubarrayBeauty first attempt

--- Array/SlidingSubaarayBeauty.ts
+++ Array/SlidingSubaarayBeauty.ts
@@ -1,3 +1,16 @@
 function getSubarrayBeauty(nums: number[], k: number, x: number): number[] {
+  const slice = nums.slice(0, k);
+  const sortedSubaaray = slice.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i <= nums.length - k; i++) {
+    const slice = nums.slice(i, k);
+    const sortedSubaaray = slice.sort((a, b) => a - b);
+    result.push(valueCalc(sortedSubaaray, x));
+  }
+  return result;
+}
 
-};
+function valueCalc(subaaray: number[], x: number) {
+  if (subaaray[0] >= 0) return 0;
+  return subaaray[x - 1];
+}
