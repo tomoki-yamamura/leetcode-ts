fe8e886e73a4d93c9a50c5828d7f752551a742df
tomoki-yamamura <progtab17@gmail.com>
2024-09-12 20:37:30 +0800

    sum

--- Array/Longest SubstringWithoutRepeatingCharacters.ts
+++ Array/Longest SubstringWithoutRepeatingCharacters.ts
@@ -1,3 +1,23 @@
 function lengthOfLongestSubstring(s: string): number {
+  let result = 0;
+  for(const letter of s) {
     
+  }
 };
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
