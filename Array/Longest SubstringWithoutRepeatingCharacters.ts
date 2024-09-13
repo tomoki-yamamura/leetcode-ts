4735a3ffd005a93fd62f5a85e7345bc3643faedc
tomoki-yamamura <progtab17@gmail.com>
2024-09-13 23:19:33 +0800

    sum

--- Array/Longest SubstringWithoutRepeatingCharacters.ts
+++ Array/Longest SubstringWithoutRepeatingCharacters.ts
@@ -1,8 +1,19 @@
 function lengthOfLongestSubstring(s: string): number {
-  let result = 0;
-  for(const letter of s) {
-    
+  let left = 0;
+  let maxLength = 0;
+  let charSet = new Set();
+
+  for (let right = 0; right < s.length; right++) {
+      while (charSet.has(s[right])) {
+          charSet.delete(s[left]);
+          left++;
+      }
+
+      charSet.add(s[right]);
+      maxLength = Math.max(maxLength, right - left + 1);
   }
+
+  return maxLength;    
 };
 
 // Example 1:
