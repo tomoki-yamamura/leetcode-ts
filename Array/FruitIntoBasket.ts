653645c7c2d4f6292d46499631fafc05e27079b2
tomoki-yamamura <progtab17@gmail.com>
2024-09-14 21:42:35 +0800

    totalFruit

--- Array/FruitIntoBasket.ts
+++ Array/FruitIntoBasket.ts
@@ -1,29 +1,21 @@
 // console.log(totalFruit([1,0,1,4,1,4,1,2,3]));
 
-
 export function totalFruit(fruits: number[]): number {
-  let left = 0;
-  let maxLength = 0;
-  const map = new Map();
-  let point = 0;
-
-  for (let right = 0; right < fruits.length; right++) {
-    if (!map.has(fruits[right]) && map.size !== 2) {
-      map.set(fruits[right], 1);
-    } else if (map.has(fruits[right]) && map.size <= 2) {
-      map.set(fruits[right], map.get(fruits[right])+1)
-    } else {
-      const sum = Array.from(map.values()).reduce(
-        (acc, value) => acc + value,
-        0
-      );
-      maxLength = Math.max(maxLength, sum);
-      const firstKey = map.keys().next().value;
-      map.delete(firstKey);
-      map.set(fruits[right], 1);
-    }
+  const hashMap = new Map();
+  let left = 0, maxLen = 0;
+for (let right = 0; right < fruits.length; right++) {
+  const rightFruit = fruits[right];
+      hashMap.set(rightFruit, hashMap.get(rightFruit) + 1 || 0);
+      while (hashMap.size > 2) {
+          const leftFruit = fruits[left];
+          if (hashMap.get(leftFruit) === 0) {
+              hashMap.delete(leftFruit);
+          } else {
+              hashMap.set(leftFruit, hashMap.get(leftFruit) - 1);
+          }
+          left++;
+      }
+      maxLen = Math.max(maxLen, right - left + 1);
   }
-  const sum = Array.from(map.values()).reduce((acc, value) => acc + value, 0);
-  maxLength = Math.max(maxLength, sum);
-  return maxLength;
+  return maxLen;
 }
