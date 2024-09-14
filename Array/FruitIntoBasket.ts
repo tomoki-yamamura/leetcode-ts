bcda5bd0a588250144fdac098a1450bb40082bd1
tomoki-yamamura <progtab17@gmail.com>
2024-09-14 21:48:14 +0800

    fruits

--- Array/FruitIntoBasket.ts
+++ Array/FruitIntoBasket.ts
@@ -2,20 +2,21 @@
 
 export function totalFruit(fruits: number[]): number {
   const hashMap = new Map();
-  let left = 0, maxLen = 0;
-for (let right = 0; right < fruits.length; right++) {
-  const rightFruit = fruits[right];
-      hashMap.set(rightFruit, hashMap.get(rightFruit) + 1 || 0);
-      while (hashMap.size > 2) {
-          const leftFruit = fruits[left];
-          if (hashMap.get(leftFruit) === 0) {
-              hashMap.delete(leftFruit);
-          } else {
-              hashMap.set(leftFruit, hashMap.get(leftFruit) - 1);
-          }
-          left++;
+  let left = 0,
+    maxLen = 0;
+  for (let right = 0; right < fruits.length; right++) {
+    const rightFruit = fruits[right];
+    hashMap.set(rightFruit, hashMap.get(rightFruit) + 1 || 0);
+    while (hashMap.size > 2) {
+      const leftFruit = fruits[left];
+      if (hashMap.get(leftFruit) === 0) {
+        hashMap.delete(leftFruit);
+      } else {
+        hashMap.set(leftFruit, hashMap.get(leftFruit) - 1);
       }
-      maxLen = Math.max(maxLen, right - left + 1);
+      left++;
+    }
+    maxLen = Math.max(maxLen, right - left + 1);
   }
   return maxLen;
 }
