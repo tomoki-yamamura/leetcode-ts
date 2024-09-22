7eea351614d93522bf106d7209cef3e5b6fcc3c3
tomoki-yamamura <progtab17@gmail.com>
2024-09-22 18:54:43 +0800

    1979

--- Array/FindGreatestCommonDivisorofArray.ts
+++ Array/FindGreatestCommonDivisorofArray.ts
@@ -1,3 +1,12 @@
 function findGCD(nums: number[]): number {
-    
+  const max = Math.max(...nums)
+  const min = Math.min(...nums)
+  return gcd(max, min)
 };
+
+function gcd(a: number, b: number): number {
+  if(b === 0) {
+    return a
+  }
+  return gcd(b, a % b)
+}
