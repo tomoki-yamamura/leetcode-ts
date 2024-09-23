47cf65836cca469dde288f9cc1fc1c0ff541b798
tomoki-yamamura <progtab17@gmail.com>
2024-09-23 21:45:06 +0800

    78

--- Array/Subsets.ts
+++ Array/Subsets.ts
@@ -1,3 +1,15 @@
 function subsets(nums: number[]): number[][] {
-    
+  const result: number[][] = [];
+
+  function backtrack(start: number, currentSet: number[]): void {
+    result.push([...currentSet]);
+    for(let i = 0; i < nums.length; i++) {
+      currentSet.push(nums[i]);
+      backtrack(i+1, currentSet);
+      currentSet.pop();
+    }
+  }
+
+  backtrack(0, []);
+  return result
 };
