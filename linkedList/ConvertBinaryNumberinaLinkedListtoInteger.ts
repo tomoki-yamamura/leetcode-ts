689a8d3e468a99374396ab08d27628c7c8bbf85b
tomoki-yamamura <progtab17@gmail.com>
2024-09-30 20:33:23 +0800

    1290

--- linkedList/ConvertBinaryNumberinaLinkedListtoInteger.ts
+++ linkedList/ConvertBinaryNumberinaLinkedListtoInteger.ts
@@ -1,15 +1,21 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
 
 function getDecimalValue(head: ListNode | null): number {
-    
-};
+  const map: number[] = []
+  function nextNode(node: ListNode | null) {
+    if (node === null) return
+      map.push(node.val)
+      nextNode(node?.next!)
+  }
+  nextNode(head)
+  if (map.length === 0) return 0
+  const binary = map.reduce((acc, cur) => String(acc) + String(cur), "")
+  return parseInt(binary, 2)
+}
