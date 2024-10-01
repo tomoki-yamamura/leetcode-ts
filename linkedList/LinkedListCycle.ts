7fd981dfa19fdb2f36da6b00d75f5a2b015ead5f
tomoki-yamamura <progtab17@gmail.com>
2024-10-02 00:18:52 +0800

    141

--- linkedList/LinkedListCycle.ts
+++ linkedList/LinkedListCycle.ts
@@ -11,5 +11,12 @@
  */
 
 function hasCycle(head: ListNode | null): boolean {
-    
+  let slow: ListNode | null = head;
+  let fast: ListNode | null = head;
+  while(fast && fast.next) {
+    slow = slow!.next
+    fast = fast.next!.next
+    if (slow === fast) return true
+  }
+  return false
 };
