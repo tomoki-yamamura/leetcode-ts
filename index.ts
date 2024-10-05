ba5809403cb1ca7edcda15f6d2ba0271789c97fe
tomoki-yamamura <progtab17@gmail.com>
2024-10-05 16:56:41 +0800

    21

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { removeElements } from "./linkedList/RemoveLinkedListElements";
 import { ListNode } from "./linkedList/MergeTwo";
+import { mergeTwoLists } from "./linkedList/MergeTwoSorted";
 
-const list = new ListNode(1, new ListNode(2, new ListNode(6, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, null)))))))
-const resu = removeElements(list, 6)
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)))
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)))
+const resu = mergeTwoLists(list1, list2)
 console.log(resu);
--- linkedList/MergeTwoSorted.ts
+++ linkedList/MergeTwoSorted.ts
@@ -10,6 +10,33 @@
  * }
  */
 
+import { ListNode } from "./MergeTwo"
+
+// export function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+//   let dummy = new ListNode()
+//   let cur = dummy
+//   while(list1 && list2) {
+//     if (list1.val > list2.val) {
+//       cur.next = list2
+//       list2 = list2.next
+//     } else {
+//       cur.next = list1
+//       list1 = list1.next
+//     }
+//     cur = cur.next
+//   }
+//   cur.next = list1 || list2
+//   return dummy.next
+// };
+
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    
-};
+  if (!list2) return list1
+  if (!list1) return list2
+  else if (list1.val <= list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2)
+    return list1
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next)
+    return list2
+  }
+}
