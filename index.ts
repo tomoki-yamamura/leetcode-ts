57d69400ca388c9e06ea5286debbf21d85990520
tomoki-yamamura <progtab17@gmail.com>
2024-10-15 23:45:43 +0800

    23

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { addTwoNumbers } from "./linkedList/AddtoNumber";
 import { ListNode } from "./linkedList/MergeTwo";
 import { mergeTwoLists } from "./linkedList/MergeTwoSorted";
 
-const list1 = new ListNode(2, new ListNode(4, new ListNode(3)))
+const list1 = new ListNode(1, new ListNode(4, new ListNode(3)))
 const list2 = new ListNode(5, new ListNode(6, new ListNode(4)))
 const resu = addTwoNumbers(list1, list2)
 console.log(resu);
