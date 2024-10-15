96b121bf1ea32546fcb4b00ebc6edbeae97d7c8a
tomoki-yamamura <progtab17@gmail.com>
2024-10-15 23:47:25 +0800

    23

--- linkedList/Mergeksorted.ts
+++ linkedList/Mergeksorted.ts
@@ -10,9 +10,30 @@
  * }
  */
 
-// import { ListNode } from "./MergeTwo";
+import { ListNode } from "./MergeTwo";
 
 
-// function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
     
-// };
+};
+
+// Example 1:
+
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted list:
+// 1->1->2->3->4->4->5->6
+// Example 2:
+
+// Input: lists = []
+// Output: []
+// Example 3:
+
+// Input: lists = [[]]
+// Output: []
