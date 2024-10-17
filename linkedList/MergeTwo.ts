eccab1304a068374ae30b183c1ad15fbce39883d
tomoki-yamamura <progtab17@gmail.com>
2024-10-17 23:55:13 +0800

    2487

--- linkedList/MergeTwo.ts
+++ linkedList/MergeTwo.ts
@@ -7,23 +7,39 @@ export class ListNode {
   }
 }
 
-export function mergeTwoLists(
+// export function mergeTwoLists(
+//   list1: ListNode | null,
+//   list2: ListNode | null
+// ): ListNode | null {
+//   const mergedHead: ListNode = new ListNode(-1, null);
+//   let prev: ListNode = mergedHead;
+
+//   while(list1 && list2) {
+//     if(list1.val <= list2.val) {
+//       prev.next = list1
+//       list1 = list1.next
+//     } else {
+//       prev.next = list2
+//       list2 = list2.next
+//     }
+//     prev = prev.next
+//   }
+//   prev.next = list1 ? list1 : list2;
+//   return mergedHead.next;
+// }
+
+function mergeTwoLists(
   list1: ListNode | null,
   list2: ListNode | null
 ): ListNode | null {
-  const mergedHead: ListNode = new ListNode(-1, null);
-  let prev: ListNode = mergedHead;
+  if (!list1) return list2;
+  if (!list2) return list1;
 
-  while(list1 && list2) {
-    if(list1.val <= list2.val) {
-      prev.next = list1
-      list1 = list1.next
-    } else {
-      prev.next = list2
-      list2 = list2.next
-    }
-    prev = prev.next
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
   }
-  prev.next = list1 ? list1 : list2;
-  return mergedHead.next;
 }
