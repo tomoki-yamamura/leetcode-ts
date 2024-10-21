b9aa1fd5ad1fac207eaa39ca42ff808f92fbc026
tomoki-yamamura <progtab17@gmail.com>
2024-10-21 20:33:10 +0800

    2130

--- linkedList/MaximumTwinSumofaLinkedList2.ts
+++ linkedList/MaximumTwinSumofaLinkedList2.ts
@@ -1,4 +1,14 @@
 import { ListNode } from "./MergeTwo";
 
 function pairSum(head: ListNode | null): number {
+  const map: number[] = []
+  while(head) {
+    map.push(head.val)
+    head = head.next
+  }
+  let max = -Infinity
+  for(let i = 0; i < map.length; i++) {
+    max = Math.max(map[i] + map[map.length-i-1], max)
+  }
+  return max
 };
