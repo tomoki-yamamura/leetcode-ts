f063a0d42451c2d0f6f7c1a831fa3b05d61bd935
tomoki-yamamura <progtab17@gmail.com>
2024-11-01 21:28:24 +0800

    153

--- binarySearch/FindMinimuminRotatedSortedArray.ts
+++ binarySearch/FindMinimuminRotatedSortedArray.ts
@@ -1,9 +1,15 @@
-function findMin(nums: number[]): number {
+export function findMin(nums: number[]): number {
   let left: number = 0
   let right: number = nums.length - 1
   let res: number = Infinity
   while(left <= right) {
     const mid = left + Math.floor((right-left)/2)
-    
+    if (nums[mid] > nums[nums.length-1]) {
+      left = mid + 1
+    } else {
+      res = Math.min(res, nums[mid])
+      right = mid - 1
+    }
   }
+  return res
 };
