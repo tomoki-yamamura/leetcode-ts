1067e861129cb1ba1180af3b530c403ac91ef189
tomoki-yamamura <progtab17@gmail.com>
2024-11-11 20:26:03 +0800

    33

--- binarySearch/SearchinRotatedSortedArray.ts
+++ binarySearch/SearchinRotatedSortedArray.ts
@@ -1,3 +1,25 @@
 function search(nums: number[], target: number): number {
-    
-};
+  let low = 0;
+  let high = nums.length - 1;
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[low] <= nums[mid]) {
+      if (nums[low] <= target && target < nums[mid]) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && target <= nums[high]) {
+        low = mid + 1;
+      } else {
+        high = mid - 1;
+      }
+    }
+  }
+  return -1
+}
